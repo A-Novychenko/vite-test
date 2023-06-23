11a7d65447a70100274fb33c525bdf424012132f
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-06-24 02:48:32 +0300

    test 404

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
-import { defineConfig } from 'vite';
-import glob from 'glob';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: 'src',
-  build: {
-    rollupOptions: {
-      input: glob.sync('./src/*.html'),
-    },
-    outDir: '../dist',
-  },
-  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+  base: "/vite-test/",
+  plugins: [react()],
 });
